ae8ffcadcd837f22828ab4f87817f4d64afa4466
jflynn129 <jflynn129@gmail.com>
2019-01-18 15:59:13 -0500

    1182019#3

--- ssd1306_96x39_spi.cpp
+++ ssd1306_96x39_spi.cpp
@@ -47,54 +47,51 @@ void testdrawline()
   for(i=0; i<display.width(); i+=4) {
     display.drawLine(0, 0, i, display.height()-1, WHITE);
     display.display(); // Update screen with each newly-drawn line
-    delay(1);
+    sleep(2);
   }
   for(i=0; i<display.height(); i+=4) {
     display.drawLine(0, 0, display.width()-1, i, WHITE);
     display.display();
-    delay(1);
+    sleep(2);
   }
-  delay(250);
 
   display.clearDisplay();
 
   for(i=0; i<display.width(); i+=4) {
     display.drawLine(0, display.height()-1, i, 0, WHITE);
     display.display();
-    delay(1);
+    sleep(2);
   }
   for(i=display.height()-1; i>=0; i-=4) {
     display.drawLine(0, display.height()-1, display.width()-1, i, WHITE);
     display.display();
-    delay(1);
+    sleep(2);
   }
-  delay(250);
 
   display.clearDisplay();
 
   for(i=display.width()-1; i>=0; i-=4) {
     display.drawLine(display.width()-1, display.height()-1, i, 0, WHITE);
     display.display();
-    delay(1);
+    sleep(2);
   }
   for(i=display.height()-1; i>=0; i-=4) {
     display.drawLine(display.width()-1, display.height()-1, 0, i, WHITE);
     display.display();
-    delay(1);
+    sleep(2);
   }
-  delay(250);
 
   display.clearDisplay();
 
   for(i=0; i<display.height(); i+=4) {
     display.drawLine(display.width()-1, 0, 0, i, WHITE);
     display.display();
-    delay(1);
+    sleep(2);
   }
   for(i=0; i<display.width(); i+=4) {
     display.drawLine(display.width()-1, 0, i, display.height()-1, WHITE);
     display.display();
-    delay(1);
+    sleep(2);
   }
 
   sleep(2); // Pause for 2 seconds
@@ -107,7 +104,7 @@ void testdrawrect(void)
   for(int16_t i=0; i<display.height()/2; i+=2) {
     display.drawRect(i, i, display.width()-2*i, display.height()-2*i, WHITE);
     display.display(); // Update screen with each newly-drawn rectangle
-    delay(1);
+    sleep(1);
   }
 
   sleep(2);
@@ -121,7 +118,7 @@ void testfillrect(void)
     // The INVERSE color is used so rectangles alternate white/black
     display.fillRect(i, i, display.width()-i*2, display.height()-i*2, INVERSE);
     display.display(); // Update screen with each newly-drawn rectangle
-    delay(1);
+    sleep(1);
   }
 
   sleep(2);
@@ -134,7 +131,7 @@ void testdrawcircle(void)
   for(int16_t i=0; i<max(display.width(),display.height())/2; i+=2) {
     display.drawCircle(display.width()/2, display.height()/2, i, WHITE);
     display.display();
-    delay(1);
+    sleep(1);
   }
 
   sleep(2);
@@ -148,7 +145,7 @@ void testfillcircle(void)
     // The INVERSE color is used so circles alternate white/black
     display.fillCircle(display.width() / 2, display.height() / 2, i, INVERSE);
     display.display(); // Update screen with each newly-drawn circle
-    delay(1);
+    sleep(1);
   }
 
   sleep(2);
@@ -162,7 +159,7 @@ void testdrawroundrect(void)
     display.drawRoundRect(i, i, display.width()-2*i, display.height()-2*i,
       display.height()/4, WHITE);
     display.display();
-    delay(1);
+    sleep(1);
   }
 
   sleep(2);
@@ -177,7 +174,7 @@ void testfillroundrect(void)
     display.fillRoundRect(i, i, display.width()-2*i, display.height()-2*i,
       display.height()/4, INVERSE);
     display.display();
-    delay(1);
+    sleep(1);
   }
 
   sleep(2);
@@ -193,7 +190,7 @@ void testdrawtriangle(void)
       display.width()/2-i, display.height()/2+i,
       display.width()/2+i, display.height()/2+i, WHITE);
     display.display();
-    delay(1);
+    sleep(1);
   }
 
   sleep(2);
@@ -210,7 +207,7 @@ void testfilltriangle(void)
       display.width()/2-i, display.height()/2+i,
       display.width()/2+i, display.height()/2+i, INVERSE);
     display.display();
-    delay(1);
+    sleep(1);
   }
 
   sleep(2);
@@ -221,7 +218,7 @@ void testdrawchar(void)
   display.clearDisplay();
 
   display.setTextSize(1);      // Normal 1:1 pixel scale
-  display.setTextColor(WHITE); // Draw white text
+//  display.setTextColor(WHITE); // Draw white text
   display.setCursor(0, 0);     // Start at top-left corner
   display.cp437(true);         // Use full 256 char 'Code Page 437' font
 
@@ -238,35 +235,32 @@ void testdrawchar(void)
 
 void testdrawstyles(void) 
 {
-#if 0
   display.clearDisplay();
 
   display.setTextSize(1);             // Normal 1:1 pixel scale
-  display.setTextColor(WHITE);        // Draw white text
+//  display.setTextColor(WHITE);        // Draw white text
   display.setCursor(0,0);             // Start at top-left corner
-  display.println(F("Hello, world!"));
+//  display.println(F("Hello, world!"));
 
-  display.setTextColor(BLACK, WHITE); // Draw 'inverse' text
-  display.println(3.141592);
+//  display.setTextColor(BLACK, WHITE); // Draw 'inverse' text
+//  display.println(3.141592);
 
   display.setTextSize(2);             // Draw 2X-scale text
-  display.setTextColor(WHITE);
-  display.print(F("0x")); display.println(0xDEADBEEF, HEX);
+//  display.setTextColor(WHITE);
+//  display.print(F("0x")); display.println(0xDEADBEEF, HEX);
 
   display.display();
   sleep(2);
-#endif
 }
 
 void testscrolltext(void) 
 {
-#if 0
   display.clearDisplay();
 
   display.setTextSize(2); // Draw 2X-scale text
-  display.setTextColor(WHITE);
+//  display.setTextColor(WHITE);
   display.setCursor(10, 0);
-  display.println(F("scroll"));
+//  display.println(F("scroll"));
   display.display();      // Show initial text
   delay(100);
 
@@ -285,7 +279,6 @@ void testscrolltext(void)
   sleep(2);
   display.stopscroll();
   sleep(1);
-#endif
 }
 
 void testdrawbitmap(void) 
@@ -304,26 +297,23 @@ void testdrawbitmap(void)
 #define YPOS   1
 #define DELTAY 2
 
-void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) 
+void testanimate(uint8_t *bitmap, uint8_t w, uint8_t h) 
 {
-  return;
-#if 0
   int8_t f, icons[NUMFLAKES][3];
 
   // Initialize 'snowflake' positions
   for(f=0; f< NUMFLAKES; f++) {
-    icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
+//    icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
     icons[f][YPOS]   = -LOGO_HEIGHT;
-    icons[f][DELTAY] = random(1, 6);
-    print(F("x: "));
-    print(icons[f][XPOS], DEC);
-    print(F(" y: "));
-    print(icons[f][YPOS], DEC);
-    print(F(" dy: "));
-    println(icons[f][DELTAY], DEC);
+//    icons[f][DELTAY] = random(1, 6);
+//    print(F("x: "));
+//    print(icons[f][XPOS], DEC);
+//    print(F(" y: "));
+//    print(icons[f][YPOS], DEC);
+//    print(F(" dy: "));
+//    println(icons[f][DELTAY], DEC);
   }
 
-  for(;;) { // Loop forever...
     display.clearDisplay(); // Clear the display buffer
 
     // Draw each snowflake:
@@ -340,13 +330,11 @@ void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h)
       // If snowflake is off the bottom of the screen...
       if (icons[f][YPOS] >= display.height()) {
         // Reinitialize to a random position, just off the top
-        icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
+//        icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
         icons[f][YPOS]   = -LOGO_HEIGHT;
-        icons[f][DELTAY] = random(1, 6);
+//        icons[f][DELTAY] = random(1, 6);
       }
     }
-  }
-#endif
 }
 
 
@@ -365,7 +353,6 @@ void do_oled_test() {
   display.display();
   sleep(2);
 
-#if 0
   printf("Draw many lines\n");
   testdrawline();      // Draw many lines
 
@@ -393,6 +380,16 @@ void do_oled_test() {
   printf("Draw triangles (filled)\n");
   testfilltriangle();  // Draw triangles (filled)
 
+  printf("Invert display\n");
+  // Invert and restore display, pausing in-between
+  display.invertDisplay(true);
+  sleep(2);
+
+  printf("Restore display\n");
+  display.invertDisplay(false);
+  sleep(2);
+
+#if 0
   printf("Draw characters of the default font\n");
   testdrawchar();      // Draw characters of the default font
 
@@ -404,14 +401,6 @@ void do_oled_test() {
 
   printf("Draw a small bitmap image\n");
   testdrawbitmap();    // Draw a small bitmap image
-
-  printf("Invert display\n");
-  // Invert and restore display, pausing in-between
-  display.invertDisplay(true);
-  sleep(1);
-  printf("Restore display\n");
-  display.invertDisplay(false);
-  sleep(1);
 
   testanimate(logo_bmp, LOGO_WIDTH, LOGO_HEIGHT); // Animate bitmaps
 #endif
